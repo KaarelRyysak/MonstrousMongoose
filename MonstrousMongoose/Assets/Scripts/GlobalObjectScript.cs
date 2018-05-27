1001d5732cee0a6dbae7599a8b50f5ac1751ebec
Owen Biegler <moldyburrito56@gmail.com>
2018-05-26 22:19:02 -0500

    Im trying really hard dad

--- MonstrousMongoose/Assets/Scripts/GlobalObjectScript.cs
+++ MonstrousMongoose/Assets/Scripts/GlobalObjectScript.cs
@@ -3,6 +3,7 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class GlobalObjectScript : MonoBehaviour
     {
@@ -10,9 +11,17 @@
         private float playerHealth;
         private int playerDashLimit;
         private int playerDashCount;
+        private GameObject player;
+        private Canvas _canvas;
+        private Text Health;
+        private Text Dash;
+        private string healthText = "Health: ";
+        private string dashText = "Dash: ";
 
         public  GameObject character;
-        private GameObject player;
+        public Canvas canvas;
+        public Text HealthText;
+        public Text DashText;
 
         //Ensures there is only ever one GLobal Object active at a time
         private void Awake()
@@ -34,6 +43,12 @@
             playerDashLimit = 3;
             playerDashCount = 3;
             player = Instantiate<GameObject>(character);
+            player.GetComponent<CharacterMovementScript>().health = playerHealth;
+            player.GetComponent<CharacterMovementScript>().dash = playerDashCount;
+
+            _canvas = Instantiate<Canvas>(canvas);
+            Health = Instantiate<Text>(HealthText, _canvas.transform);
+            Dash = Instantiate<Text>(DashText, _canvas.transform);
         }
 
         //Gets and sets for player health and dashes
@@ -61,6 +76,12 @@
         public void setDashCount(int dashCount)
         {
             playerDashCount = dashCount;
+        }
+
+        private void Update()
+        {
+            Health.text = healthText + getHealth();
+            Dash.text = dashText + getDashCount();
         }
     }
 }
